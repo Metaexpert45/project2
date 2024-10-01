92543e348734d394151cfbb14d99fc1ec1ca713a
Metaexpert45 <workspacelvm@gmail.com>
2024-10-01 18:31:47 +0000

    Add files via upload

---
+++ home.js
@@ -0,0 +1,403 @@
+import React, { Fragment } from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import FeatureCard from '../components/feature-card'
+import ContactForm2 from '../components/contact-form2'
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container1">
+      <Helmet>
+        <title>Joyful Luminous Ram</title>
+        <meta property="og:title" content="Joyful Luminous Ram" />
+      </Helmet>
+      <div className="home-header">
+        <header
+          data-thq="thq-navbar"
+          className="navbarContainer home-navbar-interactive"
+        >
+          <span className="home-logo1">
+            <span className="home-text10">FACEBOOK</span>
+            <span className="home-text11">
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>
+              |
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>Support Centre</span>
+          </span>
+          <div data-thq="thq-navbar-nav" className="home-desktop-menu">
+            <nav className="home-links1">
+              <span className="home-nav221">
+                <span className="home-text14">
+                                                                               
+                                                                       Search
+                </span>
+                <span className="bodySmall">🔍</span>
+              </span>
+            </nav>
+          </div>
+          <div data-thq="thq-burger-menu" className="home-burger-menu">
+            <svg viewBox="0 0 1024 1024" className="home-icon10 socialIcons">
+              <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            </svg>
+          </div>
+          <div
+            data-thq="thq-mobile-menu"
+            className="home-mobile-menu1 mobileMenu"
+          >
+            <div className="home-nav1">
+              <div className="home-top">
+                <span className="logo">COMPANYXYZ</span>
+                <div data-thq="thq-close-menu" className="home-close-menu">
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    className="home-icon12 socialIcons"
+                  >
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+              <nav className="home-links2">
+                <span className="home-nav12 bodySmall">Home</span>
+                <span className="home-nav222 bodySmall">Products</span>
+                <span className="home-nav32 bodySmall">Services</span>
+                <span className="home-nav42 bodySmall">Contact</span>
+                <span className="home-nav521 bodySmall">FAQ</span>
+              </nav>
+              <div className="home-buttons">
+                <button className="buttonFlat">Login</button>
+                <button className="buttonFilled">Register</button>
+              </div>
+            </div>
+            <div>
+              <svg
+                viewBox="0 0 950.8571428571428 1024"
+                className="home-icon14 socialIcons"
+              >
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 877.7142857142857 1024"
+                className="home-icon16 socialIcons"
+              >
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 602.2582857142856 1024"
+                className="home-icon18 socialIcons"
+              >
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div className="home-hero1">
+        <div className="home-hero2 heroContainer">
+          <h1 className="home-hero-heading1">Facebook Help Desk </h1>
+          <h1 className="home-hero-heading2">
+            If you believe your account has been compromised or you need
+            assistance in securing or fixing your account, please contact our
+            support team immediately for help restoring your account and
+            ensuring its safety
+          </h1>
+          <div className="home-container2">
+            <span className="home-hero-sub-heading bodyLarge">
+              <span>
+                <span>
+                  <span>Your Company Name</span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+                <span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+              </span>
+              <span>
+                <span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+                <span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+              </span>
+            </span>
+            <div className="home-btn-group"></div>
+          </div>
+        </div>
+      </div>
+      <div className="home-features1">
+        <div className="featuresContainer">
+          <div className="home-features2">
+            <h2 className="Heading">Facebook 24x7 Helpline</h2>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/2048px-Facebook_f_logo_%282021%29.svg.png"
+              alt="image"
+              className="home-image"
+            />
+            <a href="tel:8881234567" className="home-link">
+              <h2 className="Heading">1(888) 123-4567</h2>
+            </a>
+            <div className="home-container3">
+              <span className="home-features-sub-heading bodyLarge">
+                <span>
+                  <span>
+                    <span>Explore the key features of this invoice</span>
+                    <span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                  </span>
+                  <span>
+                    <span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                    <span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                  </span>
+                </span>
+                <span>
+                  <span>
+                    <span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                    <span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                  </span>
+                  <span>
+                    <span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                    <span>
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </div>
+            <div className="home-container4">
+              <FeatureCard
+                subHeading="Includes quantity, price, and total"
+                heading1={
+                  <Fragment>
+                    <span className="home-text44">
+                      <span className="home-text45 heading3">
+                        Hacked Accounts
+                      </span>
+                      <br></br>
+                    </span>
+                  </Fragment>
+                }
+              ></FeatureCard>
+              <FeatureCard
+                heading="Account Reactivation"
+                subHeading="Brief information about the product"
+                heading1={
+                  <Fragment>
+                    <span className="home-text47">Account Reactivation</span>
+                  </Fragment>
+                }
+              ></FeatureCard>
+              <FeatureCard
+                heading="Forgot login credentials"
+                subHeading="Additional comments or instructions"
+                heading1={
+                  <Fragment>
+                    <span className="home-text48">
+                      Forgot login credentials
+                    </span>
+                  </Fragment>
+                }
+              ></FeatureCard>
+              <FeatureCard
+                heading="Company Logo"
+                subHeading="Brand identity for the invoice"
+                heading1={
+                  <Fragment>
+                    <span className="home-text49">Identity Verification</span>
+                  </Fragment>
+                }
+              ></FeatureCard>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-pricing1">
+        <div className="pricingContainer">
+          <div className="home-container5">
+            <h2 className="home-pricing-heading">
+              To get an immediate assistance, please fill out the form below 
+            </h2>
+            <span className="home-pricing-sub-heading bodyLarge">
+              <span>
+                <span>Select a plan that suits your needs and budget</span>
+              </span>
+            </span>
+          </div>
+          <ContactForm2
+            content2={
+              <Fragment>
+                <span className="home-text52 thq-body-small">
+                  Connecting you to our experts in no time.
+                </span>
+              </Fragment>
+            }
+            action={
+              <Fragment>
+                <span className="home-text53 thq-body-small">Submit</span>
+              </Fragment>
+            }
+            phone={
+              <Fragment>
+                <span className="home-text54 thq-body-small">
+                  +1 (888) 123-4567
+                </span>
+              </Fragment>
+            }
+            adress={
+              <Fragment>
+                <span className="home-text55 thq-body-small">
+                  1 Hacker Wy, Menlo Park, CA 94025, United States
+                </span>
+              </Fragment>
+            }
+            heading1={
+              <Fragment>
+                <span className="home-text56 thq-heading-2">Contact us</span>
+              </Fragment>
+            }
+            rootClassName="contact-form2root-class-name"
+          ></ContactForm2>
+          <div className="home-container6"></div>
+        </div>
+      </div>
+      <div className="home-banner"></div>
+      <div className="home-faq">
+        <div className="home-faq-container faqContainer"></div>
+      </div>
+      <div className="home-footer1">
+        <footer className="footerContainer home-footer2">
+          <div className="home-container7">
+            <span className="home-logo3">Meta Platforms, Inc.,</span>
+            <nav className="home-nav2">
+              <span>1 Hacker Wy, Menlo Park, CA 94025, United States</span>
+            </nav>
+          </div>
+          <div className="home-separator"></div>
+          <div className="home-container8">
+            <span className="bodySmall home-text57">
+              © 2023 myCompany, All Rights Reserved.
+            </span>
+            <div className="home-icon-group2">
+              <svg
+                viewBox="0 0 950.8571428571428 1024"
+                className="home-icon20 socialIcons"
+              >
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 877.7142857142857 1024"
+                className="home-icon22 socialIcons"
+              >
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 602.2582857142856 1024"
+                className="home-icon24 socialIcons"
+              >
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ question1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './question1.css'
+
+const Question1 = (props) => {
+  return <div className="question1-container"></div>
+}
+
+export default Question1
